06db1721754e987a777c6f463fdf0bfbe5b1a128
1454346617 <1454346617@qq.com>
2019-11-25 14:36:51 +0800

    '文件上传类封装'

---
+++ App/Lib/Upload/Base.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/11/25 0025
+ * Time: 上午 10:52
+ */
+
+namespace App\Lib\Upload;
+
+class Video extends Base{
+
+}
+
---
+++ App/Lib/Upload/Video.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/11/25 0025
+ * Time: 上午 10:52
+ */
