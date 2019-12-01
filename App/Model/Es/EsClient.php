8d1dc407e9f7f1c18332200728a58e525c76dc14
xuyemei <1454346617@qq.com>
2019-12-01 23:40:40 +0800

    Update EsClient.php

--- App/Model/Es/EsClient.php
+++ App/Model/Es/EsClient.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace App\Model\Es;
+use EasySwoole\Core\AbstractInterface\Singleton;
+use EasySwoole\Core\Component\Logger;
+use Elasticsearch\ClientBuilder;
+
+class EsClient{
+    use Singleton;
+
+    private $client = null;
+
+    private function __construct()
+    {
+        $conf = \Yaconf::get('Es');
+        try{
+            $this->client = ClientBuilder::create()->setHosts([$conf['host'].':'.$conf['port']])->build();
+        }catch (\Exception $e){
+            Logger::getInstance()->log('es连接失败：'.$e->getMessage());
+        }
+
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+        return $this->client->$name(...$arguments);
+    }
+}
